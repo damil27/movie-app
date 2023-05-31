a623d3f47f0eff9f418e1fa7944aaaeaf2395cd7
damin <34189426+damil27@users.noreply.github.com>
2023-05-31 10:31:20 +0100

    enviroment varaibale commented out

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    api_key: "812d443aaa2523032bd3c610c27960eb",
-  },
+  // env: {
+    // api_key: "812d443aaa2523032bd3c610c27960eb",
+  // },
   images: {
     domains: ["image.tmdb.org"],
   },
